e9c7e6e366470f0107b62e1de34cad32de7ab11b
sambartek <sbartek@purdue.edu>
2018-01-18 23:25:12 -0500

    first commit

---
+++ alarm.ino
@@ -0,0 +1,49 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const int BLOCK = 255;
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+}
+
+// main
+void loop() {
+  checkerPattern();
+}
+
+
+void checkerPattern() {
+  // set the cursor to column 0, line 1
+  int row = 0; // 0 or 1
+  int col = 0; // 0 - 15
+  int timer = 1000;
+  for(int i = 0; i < 2; i++) {
+
+    // fills screen following checkered pattern
+    for(col = 0; col < 16; col++) {
+      lcd.setCursor(col, row);
+      if(row == 0) {
+        row = 1;
+      }
+      else {
+        row = 0;
+      }
+      lcd.write(BLOCK);
+      delay(timer); 
+      timer = timer - 30;
+    }
+
+    // fills in checkered pattern
+    row++;
+  }
+  // resets screen
+  lcd.clear();
+}
+
